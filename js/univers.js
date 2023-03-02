7cc3d143475eb5e1cb643a098dce7581cb05aa84
Jansu2001 <sadnansujan23@gmail.com>
2023-03-02 21:48:34 +0600

    Alhamdulillah Firts Task Complete

--- js/univers.js
+++ js/univers.js
@@ -14,28 +14,40 @@ const displayAllData = (datas) => {
     const mainCard = document.getElementById('main-card');
 
     datas.tools.forEach(singleData => {
-        console.log(singleData.features);
+        console.log(singleData);
         const cardDiv=document.createElement('div')
         cardDiv.classList.add('card')
         cardDiv.innerHTML = `
 
         <div class="col">
-          <div class="card w-full">
+          <div class="card p-4">
             <img src="${singleData.image}" class="p-3 card-img-top" alt="...">
             <div class="card-body">
-              <h5 class="card-title text-2xl">features</h5>
-              <p class="card-text">
+              <h5 class="card-title fs-3 fw-bold ">features</h5>
+              <div >
+              <p class="mb-2 card-text fw-light">
               1. ${singleData.features[0]}
               </p>
-              <p class="card-text">
+              <p class="mb-2 card-text fw-light">
               2. ${singleData.features[1]}
               </p>
-              <p class="card-text">
+              <p class="card-text fw-light">
               3. ${singleData.features[2]}
               </p>
+              </div>
             </div>
-            <div class="card-footer">
-              <small class="text-muted">Last updated 3 mins ago</small>
+            <div class="card-footer ">
+            <div>
+            <h1 class="fw-semibold">${singleData.name}</h1>
+            </div>
+            <div class="d-flex justify-content-between">
+            <div><p><i class="fa-solid fa-calendar-days"></i> ${singleData.published_in}</p></div>
+            
+            <div>
+            <i class="fa-solid fa-arrow-right"></i>
+            </div>
+            </div>
+              
             </div>
           </div>
         </div>
