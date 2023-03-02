f42e44de6d793791bbf266ffe9362b88ebc17215
Jansu2001 <sadnansujan23@gmail.com>
2023-03-03 02:02:10 +0600

    Modal Section Done

--- js/univers.js
+++ js/univers.js
@@ -80,7 +80,7 @@ const cardModal = (id) => {
 
 const displayCardModal = (data) => {
 
-    console.log(data.data);
+    console.log(data.data.features);
     const cardModal = document.getElementById('modal-body');
 
     cardModal.innerHTML = `
@@ -88,26 +88,53 @@ const displayCardModal = (data) => {
 
     <div class="container text-center p-4">
   <div class="row align-items-start gap-2">
-    <div class="col">
-    <div>
-    <h5 class="card-title">${data.data.description} <span class="badge text-bg-warning">
+    <div  class="col">
+    <div id="left-side">
+    <h5 class="card-title fw-bold">${data.data.description} <span class="badge text-bg-warning">
     </span></h5>
-    <div class="d-flex justify-content-between">
-      <p class="text-success">${data.data.pricing[0].price}<br> ${data.data.pricing[0].plan}</p>
-      <div>${data.data.pricing[1].price}<br> ${data.data.pricing[1].plan}</div>
-      <div>${data.data.pricing[2].price}<br> ${data.data.pricing[2].plan}</div>
+    <div class="d-flex justify-content-between mt-4">
+      <p id="pricing-box" class="text-success border border fw-bold rounded">${data.data.pricing[0].price}<br> ${data.data.pricing[0].plan}</p>
+      <p id="pricing-box" class="text-warning-emphasis border border fw-bold rounded">${data.data.pricing[1].price}<br> ${data.data.pricing[1].plan}</p>
+      <p id="pricing-box" class="text-danger border border fw-bold rounded">${data.data.pricing[2].price}<br> ${data.data.pricing[2].plan}</p>
     </div>
+
+
+    <div class="d-flex justify-content-between mt-2">
+
+
+  <div>
+  <h1 class="fw-bold">features</h1>
+  <li>${data.data.features[0]}</li>
+  </div>
+  
+  
+  <div>
+  <h1 class="fw-bold">integrations</h1>
+
+  <ul>
+  <li>${data.data.integrations[0]}</li>
+  <li>${data.data.integrations[1]}</li>
+  <li>${data.data.integrations[2]}</li>
+</ul>
+
+  </div>
+
+
+
+</div>
+
+
 
     
   </div>
     </div>
     <div class="col">
-    <div class="card mb-3">
-    <img src="..." class="card-img-top" alt="...">
+    <div id="right-side" class="card p-4 ">
+    <img src="${data.data.image_link[0]}" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      <h5 class="card-title mt-4 fw-bold">${data.data.input_output_examples[0].input}</h5>
+      <p class="card-text mt-4">${data.data.input_output_examples[0].output}</p>
+      
     </div>
   </div>
   
